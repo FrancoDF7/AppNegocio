038091101d1c475166176dca83cc12dbb95cdc97
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2026-08-12 20:59:49 -0300

    feat: Crear formulario GestionUsuarios

---
+++ AppNegocio.Vista/Forms/Usuarios/GestionUsuarios.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace AppNegocio.Vista
+{
+    partial class GestionUsuarios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelSuperior = new Panel();
+            iconTitulo = new FontAwesome.Sharp.IconPictureBox();
+            lblTitulo = new Label();
+            panel1 = new Panel();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            btnCambiarImagen = new FontAwesome.Sharp.IconButton();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            panelSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconTitulo).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panelSuperior
+            // 
+            panelSuperior.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panelSuperior.BackColor = Color.FromArgb(17, 45, 78);
+            panelSuperior.Controls.Add(iconTitulo);
+            panelSuperior.Controls.Add(lblTitulo);
+            panelSuperior.Location = new Point(0, 0);
+            panelSuperior.Name = "panelSuperior";
+            panelSuperior.Size = new Size(888, 25);
+            panelSuperior.TabIndex = 0;
+            // 
+            // iconTitulo
+            // 
+            iconTitulo.Anchor = AnchorStyles.Top;
+            iconTitulo.BackColor = Color.FromArgb(17, 45, 78);
+            iconTitulo.ForeColor = Color.FromArgb(249, 247, 247);
+            iconTitulo.IconChar = FontAwesome.Sharp.IconChar.UserGear;
+            iconTitulo.IconColor = Color.FromArgb(249, 247, 247);
+            iconTitulo.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconTitulo.Location = new Point(324, -3);
+            iconTitulo.Name = "iconTitulo";
+            iconTitulo.Size = new Size(32, 32);
+            iconTitulo.TabIndex = 1;
+            iconTitulo.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Anchor = AnchorStyles.Top;
+            lblTitulo.Font = new Font("Roboto", 14.25F, FontStyle.Bold);
+            lblTitulo.ForeColor = Color.White;
+            lblTitulo.Location = new Point(44, 0);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Padding = new Padding(0, 2, 0, 0);
+            lblTitulo.Size = new Size(800, 23);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Gestión de Usuarios";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(219, 226, 239);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(btnCambiarImagen);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(888, 204);
+            panel1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(563, 60);
+            label8.Name = "label8";
+            label8.Size = new Size(105, 14);
+            label8.TabIndex = 16;
+            label8.Text = "Correo electrónico";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(565, 75);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(311, 23);
+            textBox7.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(346, 154);
+            label7.Name = "label7";
+            label7.Size = new Size(126, 14);
+            label7.TabIndex = 14;
+            label7.Text = "Confirmar contraseña";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(348, 169);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(189, 23);
+            textBox6.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(344, 106);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 14);
+            label6.TabIndex = 12;
+            label6.Text = "Apellido";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(346, 121);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(189, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(346, 60);
+            label5.Name = "label5";
+            label5.Size = new Size(112, 14);
+            label5.TabIndex = 10;
+            label5.Text = "Nombre de Usuario";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(348, 75);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(189, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(131, 154);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 14);
+            label4.TabIndex = 8;
+            label4.Text = "Contraseña";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(133, 169);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(189, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(131, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 14);
+            label3.TabIndex = 6;
+            label3.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(133, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(189, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(131, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 14);
+            label2.TabIndex = 4;
+            label2.Text = "Número Documento";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(133, 75);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // btnCambiarImagen
+            // 
+            btnCambiarImagen.BackColor = Color.White;
+            btnCambiarImagen.FlatStyle = FlatStyle.Flat;
+            btnCambiarImagen.Font = new Font("Roboto", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCambiarImagen.IconChar = FontAwesome.Sharp.IconChar.IdCardClip;
+            btnCambiarImagen.IconColor = Color.Black;
+            btnCambiarImagen.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnCambiarImagen.IconSize = 35;
+            btnCambiarImagen.Location = new Point(12, 150);
+            btnCambiarImagen.Name = "btnCambiarImagen";
+            btnCambiarImagen.Size = new Size(113, 42);
+            btnCambiarImagen.TabIndex = 2;
+            btnCambiarImagen.Text = "Cambiar imagen";
+            btnCambiarImagen.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnCambiarImagen.UseVisualStyleBackColor = false;
+            btnCambiarImagen.Click += btnCambiarImagen_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Roboto", 12F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(63, 114, 175);
+            label1.Location = new Point(130, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 19);
+            label1.TabIndex = 1;
+            label1.Text = "DATOS DEL USUARIO";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(113, 113);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.BackColor = Color.White;
+            panel2.Location = new Point(6, 210);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(876, 307);
+            panel2.TabIndex = 2;
+            // 
+            // GestionUsuarios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(63, 114, 175);
+            ClientSize = new Size(888, 522);
+            Controls.Add(panel2);
+            Controls.Add(panelSuperior);
+            Controls.Add(panel1);
+            Name = "GestionUsuarios";
+            Text = "Form1";
+            panelSuperior.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconTitulo).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelSuperior;
+        private Label lblTitulo;
+        private FontAwesome.Sharp.IconPictureBox iconTitulo;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panel2;
+        private FontAwesome.Sharp.IconButton btnCambiarImagen;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label8;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+    }
+}
